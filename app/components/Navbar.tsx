acdcd35c4aaf2271a56b9d1f933f0f93de0d9afb
AndyCG03 <andyclmentegago@gmail.com>
2026-04-19 03:56:48 -0400

    icno

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
         <a href="#" className="flex items-center gap-3 group">
           <Image
-            src="/Logo.png"
+            src="/Icono.png"
             alt="Logo"
             width={150}
             height={150}
